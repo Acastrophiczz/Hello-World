f82a29e9a2269f957c93ed4635beca11ba7d995a
Acastrophiczz <36483054+Acastrophiczz@users.noreply.github.com>
2018-02-14 13:33:18 -0400

    Add files via upload
    new one

---
+++ lab1_Ramirez_Reyes.cpp
@@ -0,0 +1,22 @@
+// This program will write the name, address and telephone
+// number of the programmer.
+// PLACE YOUR NAME HERE
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "************\n";
+	cout << "Rayniel Ramirez Albizu\n";
+	cout << "Hermanas Davilas D-21\n";
+	cout << "Bayamon, PR. 00957\n";
+	cout << "787-436-7692\n";
+	cout << "************\n";
+	cout << "\n\n"; 
+	cout << " o   o		^   ^\n ";
+	cout << "  o		V   V\n";
+	cout << " (___)		  :  \n";
+	cout << "		\\UUU/\n ";
+	cout << "\n";
+	system("pause");
+	return 0;
+}
